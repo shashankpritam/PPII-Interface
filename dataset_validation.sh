fe440d326f31c399af8d0b646d49a824ed43da93
Shashank Pritam <shashankpritam@outlook.com>
2023-06-06 18:08:11 -0500

    minor improv

--- dataset_validation.sh
+++ dataset_validation.sh
@@ -8,10 +8,28 @@
 #SBATCH --error=job.%A.err
 #SBATCH --output=job.%A.out
 
+# Create a CSV file and write the headers
+echo "PDB_ID,Model,Chain,TRP,NBR,Template_PPII,Score" > output.csv
+echo "PDB_ID,Model,Chain,TRP,NBR,Template_PPII,Score" > binding_site_output.csv
+
 # Perform the operation
 while IFS= read -r line
 do
   if [ "$line" != "" ]; then
-    python3 pp2_predictor.py "$line"
+    # Run the Python script and save the output in a variable
+    output=$(python3 pp2_predictor.py "$line")
+    
+    # Extract needed information using regex and echo to the csv file
+    if [[ "$output" =~ (For query structure )([^,]+)(, predicted binding site details are - Model = )([^,]+)(, Chain = )([^,]+)(, TRP = )([^,]+)(, NBR = )([^,]+)(Template PPII is \[')([^']+)('\], with a Score of )([^$]+) ]]; then
+      PDB_ID=${BASH_REMATCH[2]}
+      Model=${BASH_REMATCH[4]}
+      Chain=${BASH_REMATCH[6]}
+      TRP=${BASH_REMATCH[8]}
+      NBR=${BASH_REMATCH[10]}
+      Template_PPII=${BASH_REMATCH[12]}
+      Score=${BASH_REMATCH[14]}
+      echo "$PDB_ID,$Model,$Chain,$TRP,$NBR,$Template_PPII,$Score" >> output.csv
+      echo "$PDB_ID,$Model,$Chain,$TRP,$NBR,$Template_PPII,$Score" >> binding_site_output.csv
+    fi
   fi
 done < <(tail -n +2 input_database.txt)
--- pp2_pred_for_database.py
+++ pp2_pred_for_database.py
@@ -1,10 +1,58 @@
-# Change the input method on the pp2_predictor.py first before running the script where input_pdb = sys.argv[1]
-# If database search is required please run pp2_pred_for_database.py and provide the query PDB ID in input_database.txt
-# Let the input command be as sys.argv[1]. Also please provide the PDB files in database_folder for database look-up.
 import os
-with open("input_database.txt") as input_pdbs:
-    lines = input_pdbs.readlines()[1:]
-    for pdb in lines:
-        run_test = "python3 pp2_predictor.py "+pdb
-        #print(run_test)
-        os.system(run_test)
+import sys
+import subprocess
+import re
+import csv
+
+# Regular expression pattern for extracting the data
+pattern = r"For query structure (.*?), predicted binding site details are - Model = (.*?), Chain = (.*?), TRP = (.*?), NBR = (.*?). Template PPII is \['(.*?)'\], with a Score of (.*?)$"
+
+def run_pp2_predictor(pdb):
+    # Construct the command
+    command = ["python3", "pp2_predictor.py", pdb.strip()]
+
+    try:
+        # Execute the command and capture the output
+        process = subprocess.run(command, check=True, text=True, capture_output=True)
+        return process.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"pp2_predictor.py failed for PDB ID {pdb.strip()}. Error: {e}", file=sys.stderr)
+        return None
+
+def main():
+    # Check if pdb ids file is provided
+    if len(sys.argv) < 2:
+        print("Usage: python3 script.py input_database.txt", file=sys.stderr)
+        sys.exit(1)
+
+    pdb_ids_file = sys.argv[1]
+    
+    if not os.path.isfile(pdb_ids_file):
+        print(f"File {pdb_ids_file} does not exist.", file=sys.stderr)
+        sys.exit(1)
+
+    # Open the CSV files for writing
+    with open(pdb_ids_file, 'r') as file, open('output.csv', 'w', newline='') as output_csvfile, open('binding_site_output.csv', 'w', newline='') as binding_site_csvfile:
+        output_writer = csv.writer(output_csvfile)
+        binding_site_writer = csv.writer(binding_site_csvfile)
+
+        # Write the headers to the CSV files
+        headers = ["PDB_ID", "Model", "Chain", "TRP", "NBR", "Template_PPII", "Score"]
+        output_writer.writerow(headers)
+        binding_site_writer.writerow(headers)
+
+        lines = file.readlines()[1:]  # Skip header line
+        for pdb in lines:
+            if pdb.strip():  # Ignore empty lines
+                output = run_pp2_predictor(pdb)
+                if output is not None:
+                    # Extract the needed information using regex
+                    match = re.search(pattern, output)
+                    if match:
+                        data = list(match.groups())
+                        # Write the data to the CSV files
+                        output_writer.writerow(data)
+                        binding_site_writer.writerow(data)
+
+if __name__ == "__main__":
+    main()
